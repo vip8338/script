8f9e7333e73dfc77beba9cbed832907c4cdd0c01
vip8338 <110080998+vip8338@users.noreply.github.com>
2022-08-15 15:14:49 +0700

    Create rvnsolotrain.sh

---
+++ rvnsolotrain.sh
@@ -0,0 +1,15 @@
+cd /home
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/3bf863cc.pub
+sudo apt-get update
+sudo apt-get -y install cuda-drivers-510
+sudo apt-get install libcurl3 -y
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex racing
+/home/racing ./racing -a kawpow -o stratum+tcp://solo-rvn.2miners.com:7070 -u RM11VvoTyD4CeixsMLHR9ntqpaMTKHMEYL.abc -p x &
